2bfec63d92c53ab49bf24f07d5bc7ba3c49907c7
서진용[Map Cloud] <jin.suh@naverlabs.com>
2020-03-29 04:14:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ display: "flex" }}>
       <header>
         <img src="https://file.namu.moe/file/6ed7a51b113187cffa26d3c1834ec0f84e31b9d668d9b4f0c4cf3a289b51b3a86ceea5977d6a6788670e5fbc20651491" />
         <p>개발중..</p>
